0c6ee960c8e987dae6dfadd835867d8872857dcd
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-08 18:11:18 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/carnival/protasknewbb/domain/Task.java

---
+++ src/main/java/com/carnival/protasknewbb/domain/Task.java
@@ -0,0 +1,153 @@
+package com.carnival.protasknewbb.domain;
+
+import com.carnival.protasknewbb.domain.enumeration.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * A Task.
+ */
+@Entity
+@Table(name = "task")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Task extends AbstractAuditingEntity<Long> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private TaskStatus status;
+
+    @Column(name = "due_date")
+    private Instant dueDate;
+
+    @ManyToOne(optional = false)
+    @NotNull
+    private User assignedTo;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Task id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Task title(String title) {
+        this.setTitle(title);
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Task description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return this.status;
+    }
+
+    public Task status(TaskStatus status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Instant getDueDate() {
+        return this.dueDate;
+    }
+
+    public Task dueDate(Instant dueDate) {
+        this.setDueDate(dueDate);
+        return this;
+    }
+
+    public void setDueDate(Instant dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public User getAssignedTo() {
+        return this.assignedTo;
+    }
+
+    public void setAssignedTo(User user) {
+        this.assignedTo = user;
+    }
+
+    public Task assignedTo(User user) {
+        this.setAssignedTo(user);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Task) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", status='" + getStatus() + "'" +
+            ", dueDate='" + getDueDate() + "'" +
+            "}";
+    }
+}
